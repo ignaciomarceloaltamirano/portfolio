1c7c2dbf210f7ae9523c315b54931f130e468197
Ignacio Altamirano <altamiranoignaciodso@gmail.com>
2022-11-06 14:38:59 -0300

    added new project

--- utils/projects.js
+++ utils/projects.js
@@ -1,12 +1,30 @@
 export const projects = [
   {
     name: 'Ushare',
-    image: 'https://source.unsplash.com/1600x900/?beach',
+    image: 'https://source.unsplash.com/1600x900/?social',
     text:
       'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totaiusto eius aut veniam sequi sunt? Lorem ipsum dolor sit amet consectetur, adipisicing elit. provident sequi velit. Illo eum possimus odio provident sequi velit. Illo eum possimus odio.',
     technologies: 'Sass, Bootstrap, React, NextJS, Firebase',
     demo: 'https://ushare-alpha.vercel.app',
     github:
       'https://github.com/ignaciomarceloaltamirano/ushare_social_media_app',
+  },
+  {
+    name: 'Imovies',
+    image: 'https://source.unsplash.com/1600x900/?movie',
+    text:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totaiusto eius aut veniam sequi sunt? Lorem ipsum dolor sit amet consectetur, adipisicing elit. provident sequi velit. Illo eum possimus odio provident sequi velit. Illo eum possimus odio.',
+    technologies: 'Sass, Tailwind CSS, React, NextJS, Firebase',
+    demo: 'https://imovies-alta.vercel.app/',
+    github: 'https://github.com/ignaciomarceloaltamirano/imovies',
+  },
+  {
+    name: 'IAShop',
+    image: 'https://source.unsplash.com/1600x900/?shop',
+    text:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totaiusto eius aut veniam sequi sunt? Lorem ipsum dolor sit amet consectetur, adipisicing elit. provident sequi velit. Illo eum possimus odio provident sequi velit. Illo eum possimus odio.',
+    technologies: 'Sass, Bootstrap, React, NextJS, Redux JS',
+    demo: 'https://iashop.vercel.app/',
+    github: 'https://github.com/ignaciomarceloaltamirano/iashop',
   },
 ];
